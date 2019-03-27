8731c6eaa5fcaa29b22513212cd6aeec3c0fbc4f
zhengjun <565061763@qq.com>
2019-03-27 20:44:24 +0800

    system_ui

--- lib/src/system_ui.dart
+++ lib/src/system_ui.dart
@@ -1,24 +1,107 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
-class FSystemUiOverlay extends StatelessWidget {
+class FSystemUiOverlay extends StatefulWidget {
   final Widget child;
-  final SystemUiOverlayStyle style;
+
+  final bool top;
+  final Color topColor;
+  final Brightness topBrightness;
+
+  final bool bottom;
+  final Color bottomColor;
+  final Brightness bottomBrightness;
 
   FSystemUiOverlay({
     @required this.child,
-    SystemUiOverlayStyle style,
+    this.top = true,
+    this.topColor = Colors.transparent,
+    this.topBrightness,
+    this.bottom = true,
+    this.bottomColor = Colors.black,
+    this.bottomBrightness,
   })  : assert(child != null),
-        this.style = style ??
-            SystemUiOverlayStyle.light.copyWith(
-              statusBarColor: Colors.transparent,
-            );
+        assert(top != null),
+        assert(bottom != null);
+
+  SystemUiOverlayStyle newStyle() {
+    if (top || bottom) {
+      Color statusBarColor;
+      Brightness statusBarBrightness;
+      Brightness statusBarIconBrightness;
+
+      if (top) {
+        final bool isDark = _isDark(
+          brightness: topBrightness,
+          color: topColor,
+        );
+
+        assert(isDark != null);
+        statusBarColor = topColor;
+        statusBarBrightness = isDark ? Brightness.dark : Brightness.light;
+        statusBarIconBrightness = isDark ? Brightness.light : Brightness.dark;
+      }
+
+      Color systemNavigationBarColor;
+      Brightness systemNavigationBarIconBrightness;
+
+      if (bottom) {
+        final bool isDark = _isDark(
+          brightness: bottomBrightness,
+          color: bottomColor,
+        );
+
+        assert(isDark != null);
+        systemNavigationBarColor = bottomColor;
+        systemNavigationBarIconBrightness =
+            isDark ? Brightness.light : Brightness.dark;
+      }
+
+      return SystemUiOverlayStyle(
+        statusBarColor: statusBarColor,
+        statusBarBrightness: statusBarBrightness,
+        statusBarIconBrightness: statusBarIconBrightness,
+        systemNavigationBarColor: systemNavigationBarColor,
+        systemNavigationBarIconBrightness: systemNavigationBarIconBrightness,
+      );
+    }
+    return null;
+  }
+
+  bool _isDark({Brightness brightness, Color color}) {
+    if (brightness != null) {
+      return brightness == Brightness.dark;
+    }
+    if (color == null || color.alpha != 0xFF) {
+      return true;
+    }
+    return color.computeLuminance() < 0.5;
+  }
+
+  @override
+  State<StatefulWidget> createState() {
+    return _FSystemUiOverlayState();
+  }
+}
+
+class _FSystemUiOverlayState extends State<FSystemUiOverlay> {
+  SystemUiOverlayStyle _style;
+
+  @override
+  void initState() {
+    super.initState();
+    _style = widget.newStyle();
+  }
 
   @override
   Widget build(BuildContext context) {
+    if (_style == null) {
+      return widget.child;
+    }
+
     return AnnotatedRegion<SystemUiOverlayStyle>(
-      child: child,
-      value: style,
+      child: widget.child,
+      value: _style,
     );
   }
 }
