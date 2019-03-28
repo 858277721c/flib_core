9f9df623a924ea24c092e60c80eee85446d40b3f
zjun <565061763@qq.com>
2019-03-28 09:14:26 +0800

    FState

--- lib/src/core.dart
+++ lib/src/core.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 
 abstract class FState<T extends StatefulWidget> extends State<T> {
+  bool _paused = false;
+
   @mustCallSuper
   @override
   void setState(fn) {
@@ -8,4 +10,20 @@ abstract class FState<T extends StatefulWidget> extends State<T> {
       super.setState(fn);
     }
   }
+
+  @override
+  void deactivate() {
+    super.deactivate();
+    _paused = !_paused;
+
+    if (_paused) {
+      onPause();
+    } else {
+      onResume();
+    }
+  }
+
+  void onPause() {}
+
+  void onResume() {}
 }
