3c424ff530208e6d77d94055515be04629bc5b1e
zjun <565061763@qq.com>
2019-03-28 10:31:21 +0800

    FSystemUiOverlay

--- lib/src/system_ui.dart
+++ lib/src/system_ui.dart
@@ -3,7 +3,43 @@ import 'package:flutter/services.dart';
 
 class FSystemUiOverlay extends StatefulWidget {
   final Widget child;
+  final FSystemUiOverlayStyle style;
 
+  FSystemUiOverlay({
+    @required this.child,
+    this.style = const FSystemUiOverlayStyle(),
+  })  : assert(child != null),
+        assert(style != null);
+
+  @override
+  State<StatefulWidget> createState() {
+    return _FSystemUiOverlayState();
+  }
+}
+
+class _FSystemUiOverlayState extends State<FSystemUiOverlay> {
+  SystemUiOverlayStyle _style;
+
+  @override
+  void initState() {
+    super.initState();
+    _style = widget.style.build();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if (_style == null) {
+      return widget.child;
+    }
+
+    return AnnotatedRegion<SystemUiOverlayStyle>(
+      child: widget.child,
+      value: _style,
+    );
+  }
+}
+
+class FSystemUiOverlayStyle {
   final bool top;
   final Color topColor;
   final Brightness topBrightness;
@@ -12,19 +48,44 @@ class FSystemUiOverlay extends StatefulWidget {
   final Color bottomColor;
   final Brightness bottomBrightness;
 
-  FSystemUiOverlay({
-    @required this.child,
+  const FSystemUiOverlayStyle({
     this.top = true,
     this.topColor = Colors.transparent,
     this.topBrightness,
     this.bottom = true,
     this.bottomColor = Colors.black,
     this.bottomBrightness,
-  })  : assert(child != null),
-        assert(top != null),
+  })  : assert(top != null),
         assert(bottom != null);
 
-  SystemUiOverlayStyle newStyle() {
+  FSystemUiOverlayStyle copyWith({
+    bool top,
+    Color topColor,
+    Brightness topBrightness,
+    bool bottom,
+    Color bottomColor,
+    Brightness bottomBrightness,
+  }) {
+    return FSystemUiOverlayStyle(
+      top: top ?? this.top,
+      topColor: topColor ?? this.topColor,
+      topBrightness: topBrightness ?? this.topBrightness,
+      bottom: bottom ?? this.bottom,
+      bottomColor: bottomColor ?? this.bottomColor,
+      bottomBrightness: bottomBrightness ?? this.bottomBrightness,
+    );
+  }
+
+  /// 应用当前style
+  void apply() {
+    final SystemUiOverlayStyle style = build();
+    if (style != null) {
+      SystemChrome.setSystemUIOverlayStyle(style);
+    }
+  }
+
+  /// 构建一个原生的[SystemUiOverlayStyle]对象，可能返回null
+  SystemUiOverlayStyle build() {
     if (top || bottom) {
       Color statusBarColor;
       Brightness statusBarBrightness;
@@ -76,32 +137,5 @@ class FSystemUiOverlay extends StatefulWidget {
       return true;
     }
     return ThemeData.estimateBrightnessForColor(color) == Brightness.dark;
-  }
-
-  @override
-  State<StatefulWidget> createState() {
-    return _FSystemUiOverlayState();
-  }
-}
-
-class _FSystemUiOverlayState extends State<FSystemUiOverlay> {
-  SystemUiOverlayStyle _style;
-
-  @override
-  void initState() {
-    super.initState();
-    _style = widget.newStyle();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    if (_style == null) {
-      return widget.child;
-    }
-
-    return AnnotatedRegion<SystemUiOverlayStyle>(
-      child: widget.child,
-      value: _style,
-    );
   }
 }
