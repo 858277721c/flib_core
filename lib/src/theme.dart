f389aaaf7b0194d33e62c947a2fa959f066da7f9
zjun <565061763@qq.com>
2019-03-28 15:41:59 +0800

    FTheme

--- lib/src/theme.dart
+++ lib/src/theme.dart
@@ -3,24 +3,36 @@ import 'package:flutter/material.dart';
 import 'res.dart';
 import 'res_ext.dart';
 
-class FButtonThemeData extends ButtonThemeData {
-  FButtonThemeData()
-      : super(
-          minWidth: 0,
-          height: FRes.dimens().heightButton,
-          padding: EdgeInsets.symmetric(vertical: 0, horizontal: 5),
-          shape: RoundedRectangleBorder(
-            borderRadius: FBorderRadiusCorner(),
-          ),
-          buttonColor: FRes.colors().mainColor,
-          disabledColor: FRes.colors().mainColor.withOpacity(0.6),
-        );
-}
+class FTheme {
+  FTheme._();
 
-class FAppBarTheme extends AppBarTheme {
-  FAppBarTheme()
-      : super(
-          color: FRes.titleBar().backgroundColor,
-          brightness: FRes.titleBar().brightness,
-        );
+  static ThemeData themeData() {
+    return ThemeData(
+      primaryColor: FRes.colors().mainColor,
+      accentColor: FRes.colors().mainColor,
+      scaffoldBackgroundColor: FRes.colors().bgPage,
+      buttonTheme: buttonThemeData(),
+      appBarTheme: appBarTheme(),
+    );
+  }
+
+  static ButtonThemeData buttonThemeData() {
+    return ButtonThemeData(
+      minWidth: 0,
+      height: FRes.dimens().heightButton,
+      padding: EdgeInsets.symmetric(vertical: 0, horizontal: 5),
+      shape: RoundedRectangleBorder(
+        borderRadius: FBorderRadiusCorner(),
+      ),
+      buttonColor: FRes.colors().mainColor,
+      disabledColor: FRes.colors().mainColor.withOpacity(0.6),
+    );
+  }
+
+  static AppBarTheme appBarTheme() {
+    return AppBarTheme(
+      color: FRes.titleBar().backgroundColor,
+      brightness: FRes.titleBar().brightness,
+    );
+  }
 }
