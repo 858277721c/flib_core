5073c545cefb7a3ae6e39cc3cec45a5d976f5e43
zjun <565061763@qq.com>
2019-04-12 16:18:12 +0800

    FApplication

--- lib/src/core.dart
+++ lib/src/core.dart
@@ -1,10 +1,25 @@
 import 'package:flutter/material.dart';
 
+abstract class FApplication {
+  bool _initialized = false;
+
+  bool get initialized => _initialized;
+
+  Future<bool> initialize() async {
+    final bool result = await initializeImpl();
+    _initialized = true;
+    return result;
+  }
+
+  @protected
+  Future<bool> initializeImpl();
+}
+
 abstract class FState<T extends StatefulWidget> extends State<T> {
   bool _paused = false;
 
   /// 查找某个State
-  T ancestorState<T extends State>() {
+  T getState<T extends State>() {
     assert(T != State);
     if (context == null) {
       return null;
