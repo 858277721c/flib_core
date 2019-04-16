ac3202cdbc66e55327bd7eb60e67dfd348e72fb4
zjun <565061763@qq.com>
2019-04-16 18:25:47 +0800

    date time

--- lib/src/utils/date_time.dart
+++ lib/src/utils/date_time.dart
@@ -2,11 +2,24 @@ import 'package:quiver/time.dart';
 
 class FTimeUtils {
   FTimeUtils._();
+}
 
-  /// 格式化剩余时间
-  static LeftTimeModel formatLeftTime(int milliSecond) {
-    final int day = milliSecond ~/ aDay.inMilliseconds;
-    final int dayLeft = milliSecond % aDay.inMilliseconds;
+class FLeftTime {
+  final int days;
+  final int hours;
+  final int minutes;
+  final int seconds;
+
+  FLeftTime({
+    this.days,
+    this.hours,
+    this.minutes,
+    this.seconds,
+  });
+
+  factory FLeftTime.parse(int milliSeconds) {
+    final int day = milliSeconds ~/ aDay.inMilliseconds;
+    final int dayLeft = milliSeconds % aDay.inMilliseconds;
 
     final int hour = dayLeft ~/ anHour.inMilliseconds;
     final int hourLeft = dayLeft % anHour.inMilliseconds;
@@ -16,25 +29,34 @@ class FTimeUtils {
 
     final int second = minuteLeft ~/ aSecond.inMilliseconds;
 
-    return LeftTimeModel(
+    return FLeftTime(
       days: day,
       hours: hour,
       minutes: minute,
       seconds: second,
     );
   }
-}
 
-class LeftTimeModel {
-  final int days;
-  final int hours;
-  final int minutes;
-  final int seconds;
+  static String _twoDigits(int n) {
+    if (n >= 10) return "${n}";
+    return "0${n}";
+  }
 
-  LeftTimeModel({
-    this.days,
-    this.hours,
-    this.minutes,
-    this.seconds,
-  });
+  String toHHmmss({String separator = ':'}) {
+    assert(separator != null);
+
+    final int hs = hours + days * 24;
+    return _twoDigits(hs) +
+        separator +
+        _twoDigits(minutes) +
+        separator +
+        _twoDigits(seconds);
+  }
+
+  String tommss({String separator = ':'}) {
+    assert(separator != null);
+
+    final int ms = minutes + (hours * 60) + (days * 24 * 60);
+    return _twoDigits(ms) + separator + _twoDigits(seconds);
+  }
 }
