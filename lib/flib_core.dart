b58e0d2a339cb70f793b7c4c5324b89065310e8c
zjun <565061763@qq.com>
2019-04-23 15:25:44 +0800

    export 'src/state_manager.dart';

--- lib/flib_core.dart
+++ lib/flib_core.dart
@@ -8,6 +8,7 @@ export 'src/button.dart';
 export 'src/core.dart';
 export 'src/res.dart';
 export 'src/res_ext.dart';
+export 'src/state_manager.dart';
 export 'src/system_ui.dart';
 export 'src/text_style.dart';
 export 'src/theme.dart';
