919453ad0b8fe0cb919c7f91c8ae66dd996576bd
zjun <565061763@qq.com>
2019-04-24 20:04:19 +0800

    export

--- lib/flib_core.dart
+++ lib/flib_core.dart
@@ -6,6 +6,8 @@ export 'src/border_radius.dart';
 export 'src/border_side.dart';
 export 'src/button.dart';
 export 'src/core.dart';
+export 'src/lifecycle_ext/event_bus.dart';
+export 'src/lifecycle_ext/live_data.dart';
 export 'src/res.dart';
 export 'src/res_ext.dart';
 export 'src/state_manager.dart';
