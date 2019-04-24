54c1255428441956442876f465c9617a6ac87675
zjun <565061763@qq.com>
2019-04-24 20:24:54 +0800

    business

---
+++ lib/src/lifecycle_ext/business.dart
@@ -0,0 +1,40 @@
+import 'package:flib_core/src/core.dart';
+import 'package:flib_lifecycle/flib_lifecycle.dart';
+import 'package:flutter/material.dart';
+
+abstract class FBusiness {
+  FBusiness(FLifecycleOwner lifecycleOwner) {
+    if (lifecycleOwner != null) {
+      final FLifecycle lifecycle = lifecycleOwner.getLifecycle();
+      assert(lifecycle != null);
+      if (lifecycle.getCurrentState() == FLifecycleState.destroyed) {
+        throw Exception('lifecycle is destroyed');
+      }
+
+      lifecycle.addObserver((event, lifecycle) {
+        onLifecycleEvent(event);
+      });
+    }
+  }
+
+  void onLifecycleEvent(FLifecycleEvent event) {}
+
+  void onDestroy() {}
+}
+
+abstract class FBusinessState<T extends StatefulWidget, B extends FBusiness>
+    extends FState<T> {
+  final B business;
+
+  FBusinessState(this.business);
+
+  @protected
+  @mustCallSuper
+  @override
+  void initState() {
+    super.initState();
+    if (B == FBusiness) {
+      throw Exception('Generics "B" are not specified');
+    }
+  }
+}
