78cf827c40a5487aae07c4a7c2db3301b3949b79
zjun <565061763@qq.com>
2019-04-25 11:10:40 +0800

    _LazyObserverWrapper 构造方法泛型缺失

--- lib/src/lifecycle_ext/live_data.dart
+++ lib/src/lifecycle_ext/live_data.dart
@@ -113,7 +113,7 @@ class _LazyObserverWrapper<T> extends _ObserverWrapper<T> {
   bool _changed = false;
 
   _LazyObserverWrapper({
-    FLiveDataObserver observer,
+    FLiveDataObserver<T> observer,
     FLiveData liveData,
     FLifecycle lifecycle,
   }) : super(
