87e01eaf4e5d26540d3513f8fdd70fee55a796a6
zjun <565061763@qq.com>
2019-04-25 14:42:49 +0800

    FLiveData 注释

--- lib/src/lifecycle_ext/live_data.dart
+++ lib/src/lifecycle_ext/live_data.dart
@@ -9,10 +9,12 @@ class FLiveData<T> {
 
   FLiveData(T value) : this._value = value;
 
-  /// 返回保存的值
+  /// 返回当前值
   T get value => _value;
 
   /// 设置值
+  ///
+  /// 当前值和新设置的值不相等(!=)的时候会通知观察者
   set value(T newValue) {
     if (_value != newValue) {
       _value = newValue;
@@ -26,7 +28,7 @@ class FLiveData<T> {
     }
 
     final List<_ObserverWrapper<T>> list =
-    _mapObserver.values.toList(growable: false);
+        _mapObserver.values.toList(growable: false);
 
     list.forEach((item) {
       item.notifyValue(_value);
@@ -34,38 +36,63 @@ class FLiveData<T> {
   }
 
   /// 添加观察者
+  ///
+  /// 添加之后不会自动移除，如果要移除观察者需要调用[removeObserver]方法
+  ///
+  /// - [observer] 观察者
+  /// - [notifyAfterAdded] 添加观察者之后，如果[_value]不为null的话是否立即通知当前添加的观察者，默认true-是
+  void addObserverForever(
+    FLiveDataObserver<T> observer, {
+    bool notifyAfterAdded = true,
+  }) {
+    addObserver(
+      observer,
+      null,
+      notifyAfterAdded: notifyAfterAdded,
+      notifyLazy: false,
+    );
+  }
+
+  /// 添加观察者
+  ///
+  /// 当[lifecycleOwner]对象分发[FLifecycleEvent.onDestroy]事件后，会自动移除观察者
+  ///
+  /// - [observer] 观察者
+  /// - [lifecycleOwner] 生命周期持有者
+  /// - [notifyAfterAdded] 添加观察者之后，如果[_value]不为null的话是否立即通知当前添加的观察者，默认true-是
+  /// - [notifyLazy] 延迟通知策略，是否生命周期状态大于等于[FLifecycleState.started]之后才通知，默认true-是
   void addObserver(
-      FLiveDataObserver<T> observer,
-      FLifecycleOwner lifecycleOwner, {
-        bool notifyAfterAdded = true,
-        bool notifyLazy = true,
-      }) {
+    FLiveDataObserver<T> observer,
+    FLifecycleOwner lifecycleOwner, {
+    bool notifyAfterAdded = true,
+    bool notifyLazy = true,
+  }) {
+    assert(observer != null);
+    assert(lifecycleOwner != null);
+    assert(notifyAfterAdded != null);
+    assert(notifyLazy != null);
+
     if (_mapObserver.containsKey(observer)) {
       return;
     }
 
-    FLifecycle lifecycle;
-    if (lifecycleOwner != null) {
-      lifecycle = lifecycleOwner.getLifecycle();
-      assert(lifecycle != null);
-    }
+    final FLifecycle lifecycle = lifecycleOwner.getLifecycle();
+    assert(lifecycle != null);
 
-    assert(notifyLazy != null);
     final _ObserverWrapper<T> wrapper = notifyLazy
         ? _LazyObserverWrapper<T>(
-      observer: observer,
-      lifecycle: lifecycle,
-      liveData: this,
-    )
+            observer: observer,
+            lifecycle: lifecycle,
+            liveData: this,
+          )
         : _ObserverWrapper<T>(
-      observer: observer,
-      lifecycle: lifecycle,
-      liveData: this,
-    );
+            observer: observer,
+            lifecycle: lifecycle,
+            liveData: this,
+          );
 
     _mapObserver[observer] = wrapper;
 
-    assert(notifyAfterAdded != null);
     if (notifyAfterAdded) {
       if (_value != null) {
         wrapper.notifyValue(_value);
@@ -74,6 +101,8 @@ class FLiveData<T> {
   }
 
   /// 移除观察者
+  ///
+  /// - [observer] 要移除的观察者
   void removeObserver(FLiveDataObserver<T> observer) {
     final _ObserverWrapper<T> wrapper = _mapObserver.remove(observer);
     if (wrapper != null) {
@@ -117,10 +146,10 @@ class _LazyObserverWrapper<T> extends _ObserverWrapper<T> {
     FLiveData<T> liveData,
     FLifecycle lifecycle,
   }) : super(
-    observer: observer,
-    liveData: liveData,
-    lifecycle: lifecycle,
-  );
+          observer: observer,
+          liveData: liveData,
+          lifecycle: lifecycle,
+        );
 
   @override
   void notifyValue(T value) {
