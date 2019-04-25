b10fbf009e491d1b25ba4233536344ca3915b288
zjun <565061763@qq.com>
2019-04-25 09:13:42 +0800

    model

--- lib/src/model.dart
+++ lib/src/model.dart
@@ -1,11 +1,27 @@
-class FPageData<T> {
-  final T data;
-
+class FPageInfo {
   /// 是否下一页的数据
   final bool isNextPage;
 
+  FPageInfo({this.isNextPage = false}) : assert(isNextPage != null);
+}
+
+/// 分页数据
+class FPageData<T> extends FPageInfo {
+  final T data;
+
   FPageData({
     this.data,
-    this.isNextPage = false,
-  }) : assert(isNextPage != null);
+    bool isNextPage = false,
+  }) : super(isNextPage: isNextPage);
+}
+
+/// 分页列表数据
+class FPageListData<T> extends FPageData<List<T>> {
+  FPageListData({
+    List<T> data,
+    bool isNextPage = false,
+  }) : super(
+          data: data ?? [],
+          isNextPage: isNextPage,
+        );
 }
